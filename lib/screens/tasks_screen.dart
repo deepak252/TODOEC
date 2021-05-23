a8e9f5887fd1a8cf67f28f14defa457761e275cd
Deepak <trojanhorsedc@gmail.com>
2021-05-23 10:51:59 +0530

    Step1 completed

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -6,18 +6,62 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body:Container(
-        padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              child: Icon(Icons.list),
-              backgroundColor: Colors.white,
-            ),
-            Text('TODODO')
-          ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(height: 12,),
+                Text(
+                  'Tododo',
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60.0
+                  ) ,
+                ),
+                Text(
+                  '12 Tasks',
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24.0
+                  ) ,
+                ),
+                
+              ],
 
-        ),
+            ),
+          ),
+          Expanded(
+              child: Container(                
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(22.0),
+                    topRight: Radius.circular(22.0)
+                  )
+                ),
+              ),
+            )
+        ],
       )
       
     );
